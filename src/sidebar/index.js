a993304a27f600d92b06060a0d90e44553a25c81
Peter Dixon-Moses <peter@searchintuition.com>
2021-03-18 12:35:31 -0400

    Decomposes tags service into `tagProvider` and `tagStore` services
    This is in preparation for simpler customization of `tagProvider`
    services beyond the current cache implemented in localStorage.
    
    - The tag service now injects a tagProvider and tagStore
    - The localStorage implementation has moved to local-tags.js
    - local-tags.js is now injected back as *both* tagProvider and tagStore.
    
    (Preserves existing functionality in client)

--- src/sidebar/index.js
+++ src/sidebar/index.js
@@ -116,6 +116,7 @@ import { FrameSyncService } from './services/frame-sync';
 import groupsService from './services/groups';
 import loadAnnotationsService from './services/load-annotations';
 import { LocalStorageService } from './services/local-storage';
+import localTagsService from './services/local-tags';
 import { PersistedDefaultsService } from './services/persisted-defaults';
 import { RouterService } from './services/router';
 import { ServiceURLService } from './services/service-url';
@@ -161,6 +162,8 @@ function startApp(config, appEl) {
     .register('streamer', streamerService)
     .register('streamFilter', StreamFilter)
     .register('tags', TagsService)
+    .register('tagProvider', localTagsService)
+    .register('tagStore', localTagsService)
     .register('threadsService', ThreadsService)
     .register('toastMessenger', ToastMessengerService)
     .register('store', store);
